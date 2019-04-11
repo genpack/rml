87d9d53176d625fd98196bd5ac7cdca6f7e84d5b
Nicholas Berta <45607424+genpack@users.noreply.github.com>
2019-04-11 17:28:57 +1000

    Segmenter modules added, genetic boosters modified

--- R/transformers.R
+++ R/transformers.R
@@ -51,8 +51,7 @@ SMBINNING = setRefClass('SMBINNING', contains = "MODEL",
 
       # predict acts like transformer
       predict = function(X){
-        callSuper()
-        X  = transform(X)
+        X  = callSuper(X)
         ns = names(objects$binners)
         ds = X[, ns]
         for(ft in ns){
@@ -64,25 +63,106 @@ SMBINNING = setRefClass('SMBINNING', contains = "MODEL",
     )
 )
 
+# Replaces categorical features with class ratios associated with each category
+SEGMENTER.RATIO = setRefClass('SEGMENTER.RATIO', contains = 'MODEL',
+   methods = list(
+     initialize = function(settings = list(), ...){
+       callSuper(settings = settings, ...)
+       type     <<- 'Class-Ratio Segmenter'
+     },
+     fit = function(X, y, do_transform = T){
+       if(!fitted){
+         if(do_transform){X = transform(X, y)}
+         objects$features <<- data.frame(name = nominals(X), stringsAsFactors = F)
+         X = X[objects$features$name]
+         objects$model <<- list()
+         for(col in colnames(X)){
+           objects$model[[col]] <<- cbind(X, label = y) %>% group_by_(col) %>% summarise(ratio = mean(label))
+         }
+         fitted <<- T
+       }
+     },
+     
+     predict = function(X){
+       X = callSuper(X)
+       for(col in objects$features$name){
+         cn = 'ratio' %>% {names(.)<-'ratio' %>% paste(col, sep = '_');.}
+         X %<>% left_join(objects$model[[col]], by = col) %>% spark.rename(cn)
+       }
+       return(X)
+     }
+   )
+)
 
-CATREMOVER = setRefClass('CATREMOVER', contains = "MODEL",
-    methods = list(
-      initialize = function(settings = list(), ...){
-        callSuper(settings = settings, ...)
-        type     <<- 'Categorical Feature Remover'
-      },
-
-      fit = function(X, y){
-        if(!fitted){
-          objects$model <<- numerics(X)
-          fitted <<- TRUE
+SEGMENTER.MODEL = setRefClass('SEGMENTER.MLR', contains = 'MODEL',
+  methods = list(
+    initialize = function(settings = list(), ...){
+      callSuper(settings = settings, ...)
+      type     <<- 'Model Segmenter'
+      config$model_class  <<- settings$model_class %>% verify('character', default = 'SCIKIT.XGB')
+      config$model_config <<- settings$model_config %>% verify('list', default = list())
+      if(is.empty(name)){name <<- 'SEGMOD' %>% paste0(sample(1000:9999, 1))}
+    },
+    
+    fit = function(X, y, do_transform = T){
+      if(!fitted){
+        if(do_transform){X = transform(X, y)}
+        objects$features <<- data.frame(name = nominals(X), stringsAsFactors = F)
+        X = X[objects$features$name]
+        objects$model <<- list()
+        for(col in colnames(X)){
+          objects$model[[col]] <<- list()
+          Xcol = X %>% pull(col)
+          uval = Xcol %>% unique
+          for(val in uval){
+            vlc = as.character(val)
+            objects$model[[col]][[as.character(vlc)]] <<- new(config$model_class, settings = config$model_config)
+            wwww = which(Xcol == val)
+            if(length(wwww) > 0){
+              objects$model[[col]][[vlc]]$fit(X[wwww,], y[wwww])
+            }
+          }
         }
-      },
-
-      predict = function(X){
-        callSuper()
-        X[objects$model]
+        fitted <<- T
       }
+    },
+    
+    predict = function(X){
+      X = callSuper(X)
+      for(col in objects$features$name){
+        bibi = function(dot){
+          dot %<>% as.data.frame
+          data.frame(value = objects$model[[col]][[dot[1,col] %>% as.character]]$predict(dot), stringsAsFactors = F)
+        }
+        cn = 'value' %>% {names(.) <-name %>% paste(col, sep = '_');.}
+        df = X %>% group_by_(col) %>% do({bibi(.)}) %>% spark.rename(cn)
+        X %<>% cbind(df[,2])
+      }
+      return(X)
+    }
+  ))
+
+CATCONCATER = setRefClass('CATCONCATER', contains = "MODEL",
+   methods = list(
+     initialize = function(settings = list(), ...){
+       callSuper(settings = settings, ...)
+       type     <<- 'Categorical Features Concater'
+       if(is.empty(name)){name <<- 'CATCON' %>% paste0(sample(1000:9999, 1))}
+     },
+     
+     fit = function(X, y){
+       if(!fitted){
+         objects$features <<- data.frame(name = nominals(X), stringsAsFactors = F)
+         fitted <<- TRUE
+       }
+     },
+     
+     predict = function(X){
+       X   = callSuper(X)
+       X[objects$features$name]
+       bibi = function(x) paste(x, collapse = '-')
+       X[objects$features$name] %>% apply(1, bibi) 
+     }
    )
 )
 
@@ -96,23 +176,38 @@ DUMMIFIER = setRefClass('DUMMIFIER', contains = "MODEL",
 
                            fit = function(X, y = NULL){
                              X = transform(X, y)
+                             objects$features     <<- data.frame(name = colnames(X), stringsAsFactors = F)
                              objects$categoricals <<- nominals(X)
+                             assert(!is.empty(objects$categoricals), 'No categorical columns found for dummification!')
+                             dummies = character()
+                             for(cat in objects$categoricals){
+                               dummies %<>% c(cat %>% paste(unique(X[, cat]), sep = '_'))
+                             }
+                             ## todo: take care of remove_first_dummy and remove_most_frequent_dummy arguments
                              fitted <<- TRUE
+                             objects$dummy_columns <<- dummies
                            },
 
                            predict = function(X){
-                             # callSuper()
-                             X = transform(X)
-                             X[objects$categoricals] %>% fastDummies::dummy_cols(objects$categoricals)
+                             X = callSuper(X)
+                             X[objects$categoricals] %>% 
+                               fastDummies::dummy_cols(objects$categoricals, remove_first_dummy = FALSE, remove_most_frequent_dummy = TRUE) %>% 
+                               {.[, -(sequence(length(objects$categoricals))), drop = F]} -> res
+                             # We need to make sure the column names of the output is exactly the same as the table given in fitting
+                             comm_cols  = colnames(res) %^% objects$dummy_columns
+                             less_cols  = objects$dummy_columns %-% colnames(res)
+                             extra      = data.frame.na(nrow = nrow(res), col_names = less_cols)
+                             
+                             res[, comm_cols] %>% cbind(extra) %>% na2zero %>% {.[, objects$dummy_columns, drop = F]}
                            }
                          )
 )
 
-GENETIC.GEOMETRIC.BOOSTER = setRefClass('GENETIC.GEOMETRIC.BOOSTER', contains = 'MODEL',
+GENETIC.BOOSTER.GEOMETRIC = setRefClass('GENETIC.BOOSTER.GEOMETRIC', contains = 'MODEL',
     methods = list(
       initialize = function(settings = list(), ...){
-        callsuper(settings = settings, ...)
-        if(is.null(config$correlation_meter)){config$correlation_meter <<- cor}
+        callSuper(settings = settings, ...)
+        if(is.null(settings$metric)){config$metric <<- cor}
       },
 
       getFeatureValue = function(flist, name, dataset){
@@ -153,6 +248,46 @@ GENETIC.GEOMETRIC.BOOSTER = setRefClass('GENETIC.GEOMETRIC.BOOSTER', contains = 
           }
 
         }
-      },
+      }
     )
                                         )
+
+
+GENETIC.BOOSTER.LOGICAL = setRefClass('GENETIC.BOOSTER.LOGICAL', contains = 'MODEL',
+                                        methods = list(
+                                          initialize = function(settings = list(), ...){
+                                            callSuper(settings = settings, ...)
+                                            if(is.null(settings$metric)){config$metric <<- cross_enthropy}
+                                            if(is.null(settings$num_top_features)) {config$num_top_features <<- 1}
+                                            if(is.null(settings$cycle_births)) {config$num_top_features <<- 1000}
+                                            if(is.null(settings$cycle_survivors)) {config$num_top_features <<- 100}
+                                            if(is.null(settings$final_survivors)) {config$final_survivors <<- 1}
+                                          },
+                                          
+                                          getFeatureValue = function(name, dataset){
+                                            if(!fitted) stop(paste('from', name, 'of type', type, ':', 'Model not fitted!', '\n'))
+                                            getFeatureValue.logical(objects$flist, name, dataset)
+                                          },
+                                          
+                                          fit = function(X, y){
+                                            if(!fitted){
+                                              X               <- transform(X, y)
+                                              objects$flist   <<- genBinFeatBoost.fit(X, y, target = 0.9, epochs = 10, cycle_survivors = config$cycle_survivors, final_survivors = config$final_survivors, cycle_births = config$cycle_births, metric = config$metric)
+                                              objects$features <<- data.frame(name = objects$flist$father %U% objects$flist$father, stringsAsFactors = F)
+                                              fitted          <<- TRUE
+                                            }
+                                          },
+                                          
+                                          predict = function(X){
+                                            X   = callSuper(X)
+                                            top = objects$flist %>% rownames2Column('name') %>% distinct(correlation, .keep_all = T) %>% 
+                                              arrange(desc(correlation)) %>% head(config$num_top_features)
+                                            df = NULL
+                                            for (i in top %>% nrow %>% sequence){
+                                              df %<>% cbind(getFeatureValue(top$name[i], X))  
+                                            }
+                                            colnames(df) <- top$name
+                                            df
+                                          }
+                                        )
+)
