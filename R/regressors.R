afddef70318a7e1c89c9f8e40b6ef42e1047e2f6
Nicholas Berta <45607424+genpack@users.noreply.github.com>
2019-06-07 13:15:26 +1000

    reagressors updated

--- R/regressors.R
+++ R/regressors.R
@@ -1,52 +1,88 @@
+REGRESSOR = setRefClass('REGRESSOR', contains = "MODEL",
+ methods = list(
+   initialize = function(...){
+     callSuper(...)
+     type               <<- 'Abstract Regressor'
+     config$sig_level   <<- config$sig_level   %>% verify('numeric', domain = c(0,1), default = 0.05)
+     config$sfs.enabled <<- config$sfs.enabled %>% verify('logical', domain = c(F,T), default = F)
+     if(is.null(config$metric)){
+       config$metric <<- function(y1, y2){
+         err = (y1 - y2)^2 %>% sum
+         # den = (y_test - mean(y_test))^2 %>% sum
+         den = (y2 - mean(y2))^2 %>% sum
+         return(1.0 - min(err/den, 1.0))
+       }
+     }
+   },
+   
+   predict = function(X){
+     XORG = callSuper(X)
+     XFET = XORG[objects$features$fname]
+     XOUT = .self$model.predict(XFET)
+     colnames(XOUT) <- name %>% paste('out', sep = '_')
+     treat(XOUT, XFET, XORG)
+   },
+   
+   performance = function(X, y, metric = c('rmse', 'mae', 'medae')){
+     metric = match.arg(metric)
+     yp = predict(X) %>% pull(name %>% paste('out', sep = '_')) 
+     if(metric == 'rmse'){
+       return(rmse(yp, y))
+     }
+     if(metric == 'mae'){
+       return(mae(yp, y))
+     }
+     if(metric == 'medae'){
+       return(medae(yp, y))
+     }
+   }
+ )
+)
+
+
 # A simple linear regression model. Features with linear dependency to others, will be removed to avoid singularity.
 # feature importances are based on p-values of coefficients.
 #' @export STATS.LM
-STATS.LM = setRefClass('STATS.LM', contains = "MODEL",
+REG.LM = setRefClass('REG.LM', contains = "REGRESSOR",
    methods = list(
      initialize = function(...){
        callSuper(...)
        config$sig_level <<- config$sig_level %>% verify('numeric', domain = c(0,1), default = '0.1')
        type             <<- 'Linear Regression'
-       if(is.null(config$metric)){
-         settings$metric <- function(y1, y2){
-           err = (y1 - y2)^2 %>% sum
-           # den = (y_test - mean(y_test))^2 %>% sum
-           den = (y2 - mean(y2))^2 %>% sum
-           return(1.0 - min(err/den, 1.0))
-         }
-       }
-
      },
 
-     fit = function(X, y){
-       X     = transform(X, y)
-       forml = as.formula('y ~ ' %>% paste(paste(names(X), collapse = ' + ')))
-       objects$model <<- lm(forml, data = cbind(X, y))
+     model.fit = function(X, y){
+       objects$features <<- objects$features %>% filter(fclass %in% c('numeric', 'integer'))
+       if(config$sfs.enabled){
+         D   = cbind(X, Y = y) %>% as.matrix
+         res = evaluate(D)
+         objects$features <<- objects$features %>% filter(fname %in% res$sig.feature.names)
+       }
+       X = X[objects$features$fname]
+       forml = as.formula('y ~ ' %>% paste(paste(colnames(X), collapse = ' + ')))
+       objects$model <<- stats::lm(forml, data = cbind(X, y))
        singulars = is.na(objects$model$coefficients) %>% which %>% names
-       if(length(singulars) > 0){
-         X = X[, colnames(X) %>% setdiff(singulars)]
-         fit(X, y)
+       while(length(singulars) > 0){
+         objects$features <<- objects$features %>% filter(!(fname %in% singulars))
+         model.fit(X, y)
+         singulars = is.na(objects$model$coefficients) %>% which %>% names
        }
-       fitted <<- TRUE
-     },
-
-     get.features.name = function(){
-       names(objects$model$coefficients[-1])
+       gw = get.features.weight()
+       objects$features$importance <<- gw[objects$features$fname]
      },
 
      get.features.weight = function(){
-       if(is.null(objects$model.summary)){objects$model.summary <<- summary(objects$model)}
+       objects$model.summary <<- summary(objects$model)
        pv   = objects$model.summary$coefficients[-1, 'Pr(>|t|)']
        keep = (pv < 0.1)
        weights = pv
        weights[!keep] <- 0
        weights[keep]  <- 1.0 - weights[keep]/0.1
-       return(weights/sum(weights))
+       return(weights)
      },
 
-     predict = function(X){
-       X = transform(X)
-       objects$model %>% stats::predict(X)
+     model.predict = function(X){
+       objects$model %>% stats::predict(X) %>% as.data.frame
      },
 
      get.performance.fit = function(){
@@ -96,3 +132,28 @@ STATS.LM = setRefClass('STATS.LM', contains = "MODEL",
      }
    )
 )
+
+# REG.TAYLOR = setRefClass('REG.TAYLOR', contains = 'MODEL', methods = list(
+#   initialize = function(...){
+#     callSuper(...)
+#     type               <<- 'Taylor Booster'
+#     config$model_class <<- 'REG.LM'
+#   },
+#   
+#   model.fit = function(X, y){
+#     objects$features <<- objects$features %>% filter(fclass %in% c('numeric', 'integer'))
+#     X = X[objects$features$fname]
+#     
+#     mdl <- new(config$model_class, config = config$model_config, rfe.enabled = T, sfs.enabled = T)
+#     mdl$fit(X, y)
+#     Xp = X[mdl$objects$features$fname]
+#     for(col in mdl$objects$features$fname){
+#       Xi = X %>% as.matrix %>% apply(2, function(v) X[,col]*v) %>% {colnames(.) <- colnames(.) %>% paste('X', col, sep = '_')}
+#       Xi = cbind(Xp, Xi)
+#       mdl <- new(config$model_class, config = config$model_config, rfe.enabled = T, sfs.enabled = T)
+#       mdl$fit(Xi, y)
+#       Xp = Xi[mdl$objects$features$fname]
+#     }
+#     object
+#   }
+# ))
